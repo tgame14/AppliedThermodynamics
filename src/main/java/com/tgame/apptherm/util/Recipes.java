905969881f3b9a197ad9533928688c33338395a7
tgame14 <tgameBusiness@gmail.com>
2014-01-14 20:59:49 +0200

    Added recipes for all Functional Blocks.

--- src/main/java/com/tgame/apptherm/util/Recipes.java
+++ src/main/java/com/tgame/apptherm/util/Recipes.java
@@ -21,16 +21,42 @@ public class Recipes {
 
 		/* Block recipes */
 		GameRegistry.addRecipe(new ItemStack(Blocks.airIntake), "X X", "ZYZ",
-				"XXX", 'X', Materials.matSilicon, 'Y', Materials.matFluxDust.copy(),
-				'Z', ironIngot);
+				"XXX", 'X', Materials.matSilicon, 'Y',
+				Materials.matFluxDust.copy(), 'Z', ironIngot);
 
-		GameRegistry.addRecipe(new ItemStack(Blocks.liquidCooler), "XYX",
+		GameRegistry.addRecipe(new ItemStack(Blocks.reactChamber), "XYX",
 				"ZXZ", "XWX", 'X', ironIngot, 'Y', new ItemStack(
 						Block.fenceIron), 'Z', new ItemStack(Item.bucketWater),
 				'W', Materials.matConversionMatrix.copy());
 
 		GameRegistry.addRecipe(new ItemStack(Blocks.exhaustME), "X X", "XZX",
 				"YXY", 'X', ironIngot, 'Z', Materials.matProcessorBasic.copy());
-	}
 
+		GameRegistry.addRecipe(new ItemStack(Blocks.fanBoxCasing), "XYX",
+				"YZY", "XYX", 'X', Materials.matFluxCrystal.copy(), 'Y',
+				Item.ingotGold, 'Z', ironIngot);
+
+		GameRegistry.addRecipe(new ItemStack(Blocks.fluidPort), "XYX", "YZY",
+				"XYX", 'X', ironIngot, 'Y', Materials.matFluxDust.copy(), 'Z',
+				new ItemStack(Item.bucketWater));
+
+		GameRegistry.addRecipe(new ItemStack(Blocks.internalTank), "XYX",
+				"YZY", "XYX", 'X', ironIngot, 'Y', new ItemStack(Block.glass),
+				'Z', new ItemStack(Item.bucketWater));
+
+		GameRegistry.addRecipe(new ItemStack(Blocks.liquidCooler), "ZXZ",
+				"XYX", "ZXZ", 'Z', Materials.matSilicon.copy(), 'X', ironIngot,
+				'Y', Materials.matProcessorAdvanced.copy());
+
+		GameRegistry
+				.addRecipe(new ItemStack(Blocks.mePort), "XYX", "YZY", "XYX",
+						'X', ironIngot, 'Y', Materials.matFluxCrystal.copy(),
+						'X', ironIngot, 'Z',
+						appeng.api.Blocks.blkColorlessCable.copy());
+
+		GameRegistry.addRecipe(new ItemStack(Blocks.vent), "XYX", "YZY", "XYX",
+				'X', ironIngot, 'Y', (Block.fenceIron), 'Z', new ItemStack(
+						Block.gravel));
+
+	}
 }
