36028f52be70d34a1b118b1bc438d7a083811219
tgame14 <tgameBusiness@gmail.com>
2014-01-20 22:06:27 +0200

    fixed unneeded deprication

--- src/main/java/com/tgame/apptherm/tileentities/TileEntities.java
+++ src/main/java/com/tgame/apptherm/tileentities/TileEntities.java
@@ -15,45 +15,52 @@ import com.tgame.apptherm.tileentities.liquidcooler.TileEntityLiquidCoolingCasin
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@Deprecated
 public class TileEntities {
 
+	
 	public static void init() {
-
-		GameRegistry.registerTileEntity(TileEntityLiquidME.class,
-				BlockInfo.LIQUID_TE_KEY);
-
-		GameRegistry.registerTileEntity(TileEntityMEExhaust.class,
-				BlockInfo.EXHAUST_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityLiquidCooler.class,
-				BlockInfo.LIQUICOOL_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntitySimpleFan.class,
-				BlockInfo.INTAKE_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityHeatMonitor.class,
-				BlockInfo.MONITOR_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityEAS.class,
-				BlockInfo.EAS_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityEater.class,
-				BlockInfo.EATER_TE_KEY);
-		
-		GameRegistry.registerTileEntity(TileEntityHeatVent.class,
-				BlockInfo.VENT_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityMEPort.class,
-				BlockInfo.MEPORT_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityFluidPort.class,
-				BlockInfo.FLUIDPORT_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityInternalTank.class,
-				BlockInfo.INTTANK_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityReactionChamber.class,
-				BlockInfo.REACT_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityLiquidCoolingCasing.class,
-				BlockInfo.COOLER_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityCoolerWall.class,
-				BlockInfo.COOLWALL_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityExchange.class,
-				BlockInfo.EXCHANGE_TE_KEY);
-		GameRegistry.registerTileEntity(TileEntityHeatPort.class,
-				BlockInfo.HEATPORT_TE_KEY);
-
+		/*
+		 * 
+		 * 
+		 * GameRegistry.registerTileEntity(TileEntityLiquidME.class,
+		 * BlockInfo.LIQUID_TE_KEY);
+		 * 
+		 * GameRegistry.registerTileEntity(TileEntityMEExhaust.class,
+		 * BlockInfo.EXHAUST_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityLiquidCooler.class,
+		 * BlockInfo.LIQUICOOL_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntitySimpleFan.class,
+		 * BlockInfo.INTAKE_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityHeatMonitor.class,
+		 * BlockInfo.MONITOR_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityEAS.class,
+		 * BlockInfo.EAS_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityEater.class,
+		 * BlockInfo.EATER_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityFanBoxCasing.class,
+		 * BlockInfo.FANBOX_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityHeatVent.class,
+		 * BlockInfo.VENT_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityMEPort.class,
+		 * BlockInfo.MEPORT_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityFluidPort.class,
+		 * BlockInfo.FLUIDPORT_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityInternalTank.class,
+		 * BlockInfo.INTTANK_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityReactionChamber.class,
+		 * BlockInfo.REACT_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityLiquidCoolingCasing.class,
+		 * BlockInfo.COOLER_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityCoolerWall.class,
+		 * BlockInfo.COOLWALL_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityExchange.class,
+		 * BlockInfo.EXCHANGE_TE_KEY);
+		 * GameRegistry.registerTileEntity(TileEntityHeatPort.class,
+		 * BlockInfo.HEATPORT_TE_KEY);
+		 * 
+		 * 
+		 */
 	}
 
 }
